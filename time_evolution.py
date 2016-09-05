106b5d5a95adf0d7f84000adec4fb8fd914469fa
Sahir Khan <sahir86@gmail.com>
2016-09-05 19:05:28 +0200

    Runge Kutta & Forward Euler Codes Combined.
    - Functions added to compute solutions with both types of schemes.
    - Solutions from both schemes plotted on same graph.
    - Label for dry static energy corrected to include \hat{s}.
    - Used h_array.copy() to get rid of problem where one results array
      overwrote the other.
    - Noticed a mistake regarding cross referencing of results arrays within
      Runge Kutta time stepping loop. Made changes. This is an amended
      commit.

--- time_evolution.py
+++ time_evolution.py
@@ -33,22 +33,48 @@ def s_init_calc(s_0,delta_s):
     s_init = s_0 + delta_s / 2.0
     return s_init
 
-def h_n_plus_one_calc(alpha,delta_f,delta_s,s_n,s_0,D,h_n,delta_t):
+def h_n_plus_one_euler(alpha,delta_f,delta_s,s_n,s_0,D,h_n,delta_t):
     h_n_plus_one = ((alpha * delta_f) / (delta_s - s_n + s_0) - D*h_n) * delta_t + h_n 
     return h_n_plus_one
 
-def s_n_plus_one_calc(delta_f,alpha,V,s_n,s_0,h_n,delta_t):
+def s_n_plus_one_euler(delta_f,alpha,V,s_n,s_0,h_n,delta_t):
     s_n_plus_one = ((delta_f*(alpha - 1) - V*(s_n - s_0)) / h_n) * delta_t + s_n
     return s_n_plus_one
 
 def time_steps_calc(t_max,delta_t):
     time_steps = int( t_max / delta_t)
     return time_steps
+
+
+# Runge Kutta Functions for h and s
+def h_n_plus_one_runge(alpha,delta_f,delta_s,s_n,s_0,D,h_n,delta_t):
+
+    k1 = ((alpha * delta_f) / (delta_s - s_n + s_0) - D*h_n)
+    k2 = ((alpha * delta_f) / (delta_s - s_n + s_0) - D*(h_n + k1 * delta_t / 2))
+    k3 = ((alpha * delta_f) / (delta_s - s_n + s_0) - D*(h_n + k2 * delta_t / 2))
+    k4 = ((alpha * delta_f) / (delta_s - s_n + s_0) - D*(h_n + k3 * delta_t ))
+
+    h_n_plus_one = h_n + (delta_t / 6) * (k1 + 2*k2 + 2*k3 + k4)
+
+    return h_n_plus_one
+
+
+def s_n_plus_one_runge(delta_f,alpha,V,s_n,s_0,h_n,delta_t):
+
+    k1 = ((delta_f*(alpha - 1) - V*(s_n - s_0)) / h_n) 
+    k2 = ((delta_f*(alpha - 1) - V*((s_n + k1 * delta_t / 2) - s_0)) / h_n)
+    k3 = ((delta_f*(alpha - 1) - V*((s_n + k2 * delta_t / 2) - s_0)) / h_n)
+    k4 = ((delta_f*(alpha - 1) - V*((s_n + k3 * delta_t ) - s_0)) / h_n)
+
+    s_n_plus_one = s_n + (delta_t / 6) * (k1 + 2*k2 + 2*k3 + k4)
+
+    return s_n_plus_one
+
 # Lowest and highest values of alpha. Note that alpha should be > 0 and < 3.5. Never equal to 3.5. 
 
 # For time evolving problem. Ask users for alpha and initial h values.
 alpha = float(raw_input("Please enter a value for alpha:> ") or "1.0") # If the user enters nothing, use 1.5 as the default value.  
-h_init = float(raw_input("Please enter an initial value for h:> ") or "800.0") # If nothing is entered, use 10m for the initial height
+h_init = float(raw_input("Please enter an initial value for h:> ") or "10.0") # If nothing is entered, use 10m for the initial height
 t_max = int(raw_input("Please enter a maximum time value:> ") or "2000000")
 delta_t = float(raw_input("Please enter a time step size:> ") or "50.0")
 #alpha = np.linspace(start_alpha,end_alpha,100) # Create an array of alpha values using the input values taken from the user. Note that 100 divisions are used. 
@@ -74,17 +100,26 @@ s_init = s_init_calc(s_0, params.delta_s)
 h_array[0] = h_init
 s_array[0] = s_init
 
+h_array_runge = h_array.copy()
+s_array_runge = s_array.copy()
+
+
 for i in range(1,time_steps):
 
     # Compute variables using functions defined above. 
-    h_array[i] = h_n_plus_one_calc(alpha,params.delta_f,params.delta_s,s_array[i-1],s_0,params.D,h_array[i-1],delta_t) 
+
+    h_array[i] = h_n_plus_one_euler(alpha,params.delta_f,params.delta_s,s_array[i-1],s_0,params.D,h_array[i-1],delta_t) 
    
-    s_array[i] = s_n_plus_one_calc(params.delta_f,alpha,params.V,s_array[i-1],s_0,h_array[i-1],delta_t)   
+    s_array[i] = s_n_plus_one_euler(params.delta_f,alpha,params.V,s_array[i-1],s_0,h_array[i-1],delta_t)   
+
+    h_array_runge[i] = h_n_plus_one_runge(alpha,params.delta_f,params.delta_s,s_array_runge[i-1],s_0,params.D,h_array_runge[i-1],delta_t) 
+   
+    s_array_runge[i] = s_n_plus_one_runge(params.delta_f,alpha,params.V,s_array_runge[i-1],s_0,h_array_runge[i-1],delta_t)   
 
 # For debugging purposes - print out the solution arrays for inspection.
-np.set_printoptions(precision=3) # Controls the precision of values in the numpy arrays.
-print "h_array equals: " , h_array
-print "s_array equals: " , (s_array / 1000.0) # Presents values in kJ/kg
+#np.set_printoptions(precision=3) # Controls the precision of values in the numpy arrays.
+#print "h_array equals: " , h_array
+#print "s_array equals: " , (s_array / 1000.0) # Presents values in kJ/kg
     
 # Graphing commands
 labelstr_alpha =str(r"$\alpha =$ ") + "%.2f" % alpha
@@ -93,7 +128,8 @@ combined_labelstr = labelstr_alpha + "  " + labelstr_delta_f
 #pl.figure(1) # Switch to figure 1.
 pl.figure("h_vs_time")
     
-pl.plot(time_array,h_array,label=combined_labelstr)
+pl.plot(time_array,h_array,label=combined_labelstr + " " + "Euler")
+pl.plot(time_array,h_array_runge,label=combined_labelstr + " " + "Runge Kutta")
 pl.title("Steady State Height vs Time" ,fontsize=26)
 pl.xlabel(r"Time", fontsize=24)
 pl.ylabel(r"$\mathrm{h} \quad \mathrm{[m]}$", fontsize=24)
@@ -103,10 +139,13 @@ pl.legend(ncol=1,loc = 'upper left')
 pl.figure("s_vs_time") 
 
 s_array_kJ = s_array / 1000.0 # Convert values in Joules/kg to kilo Joules/kg. 
-pl.plot(time_array,s_array_kJ,label=combined_labelstr)
+s_array_kJ_runge = s_array_runge / 1000.0 # Convert values in Joules/kg to kilo Joules/kg. 
+
+pl.plot(time_array,s_array_kJ,label=combined_labelstr + " " + "Euler")
+pl.plot(time_array,s_array_kJ_runge,label=combined_labelstr + " " + "Runge Kutta")
 pl.title("Steady State Dry Static Energy vs Time", fontsize=26)
 pl.xlabel(r"Time", fontsize=24)
-pl.ylabel(r"$\mathrm{s} \quad \mathrm{[kJ kg^{-1}]}$", fontsize=24)
+pl.ylabel(r"$\hat{\mathrm{s}} \quad \mathrm{[kJ kg^{-1}]}$", fontsize=24)
 pl.legend(ncol=1,loc = 'upper left')
 
  
